0632055af53176d339278ba7f2267456daaa6da4
Lana <tiushkievich00@mail.ru>
2022-04-14 21:30:25 +0300

    feat: some task

---
+++ index.js
@@ -0,0 +1,21 @@
+//zadacha1
+// let info = "We couldn't verify you mother's maiden name.";
+// let intro = 'Here is important information about your account security.';
+
+// let firstName = ' Joffrey!';
+// let greeting = 'Hello,';
+// let what = greeting + firstName;
+// let that = intro +'\n' + info;
+// console.log(what);
+// console.log(that);
+
+// zadacha2
+// let firstNumber = '11';
+// let secondNumber = '-100';
+// let itogo = firstNumber * secondNumber;
+// console.log(itogo);
+
+let king = "King Balon the 6th";
+let castle = "6";
+let room = "17";
+console.log(king + " has " + castle * room + " rooms.");
